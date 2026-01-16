8f0f3c822c8c6ca26a71e77cb4ad7873249e10c1
Дмитрий Кузьмин <kuzmin.dmtry@gmail.com>
2026-01-16 17:46:14 +0300

    правки для  деплоя

--- bot.py
+++ bot.py
@@ -1,9 +1,12 @@
 import asyncio
+import os
+
 from aiogram import Bot, Dispatcher
 from config import TOKEN
 from handlers import setup_handlers
 from middlewares import LoggingMiddleware
 from database import init_db
+from aiohttp import web
 
 # Создаем экземпляры бота и диспетчера
 bot = Bot(token=TOKEN)
@@ -13,8 +16,25 @@ dp = Dispatcher()
 dp.message.middleware(LoggingMiddleware())
 setup_handlers(dp)
 
+async def health(request):
+    return web.Response(text="OK")
+
+async def start_http_server():
+    app = web.Application()
+    app.router.add_get("/", health)
+
+    runner = web.AppRunner(app)
+    await runner.setup()
+
+    port = int(os.getenv("PORT", 10000))  # Render требует порт
+    site = web.TCPSite(runner, "0.0.0.0", port)
+
+    await site.start()
+    print(f"HTTP сервер запущен на порту {port}")
+
 async def main():
     await init_db()
+    await start_http_server()
     print("Бот запущен!")
     await dp.start_polling(bot)
 
